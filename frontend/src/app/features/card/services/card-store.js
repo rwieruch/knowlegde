49aa2ce2c05af22b38946d841bcb47270b5f4d6b
Robin Wieruch <wrobin@gmx.net>
2015-05-25 18:50:11 +0200

    feat(deck): navigation for decks

--- frontend/src/app/features/card/services/card-store.js
+++ frontend/src/app/features/card/services/card-store.js
@@ -14,8 +14,8 @@ function CardStore() {
 
     // TODO init empty
     var itemsById = {
-        1: { id: "1", question: "What is functional programming?", answer: "Foo" },
-        2: { id: "2", question: "What is a closure?", answer: "Bar" }
+        1: { id: "1", question: "What is functional programming?", answer: "Funktionale Programmierung ist ein Programmierparadigma, bei dem Programme ausschließlich aus Funktionen bestehen. Dadurch wird bewusst auf die aus der imperativen Programmierung bekannten Nebenwirkungen verzichtet." },
+        2: { id: "2", question: "What is a closure?", answer: "JavaScript variables can belong to the local or global scope. Private variables can be made possible with closures." }
     };
 
     this.add = add;
